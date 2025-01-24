4785215a5f1dc47067b3e868522ffd46477f45e1
Ao Li <leeleo3x@gmail.com>
2025-01-23 22:01:16 -0500

    Fix gradle example.

---
+++ fray-gradle-example/src/test/java/BankAccountTest.java
@@ -0,0 +1,40 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.pastalab.fray.junit.junit5.FrayTestExtension;
+import org.pastalab.fray.junit.junit5.annotations.ConcurrencyTest;
+
+@ExtendWith(FrayTestExtension.class)
+public class BankAccountTest {
+
+
+    public void myBankAccountTest() throws InterruptedException {
+        BankAccount account = new BankAccount();
+        Thread t1 = new Thread(() -> {
+            account.withdraw(500);
+            assert(account.balance.get() > 0);
+        });
+        Thread t2 = new Thread(() -> {
+            account.withdraw(700);
+            assert(account.balance.get() > 0);
+        });
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+    }
+
+
+    @ConcurrencyTest(
+            iterations = 1000
+    )
+    public void runTestUsingFray() throws InterruptedException {
+        myBankAccountTest();
+    }
+
+    @Test
+    public void runTestUsingJunit() throws InterruptedException {
+        for (int i = 0; i < 10000; i++) {
+            myBankAccountTest();
+        }
+    }
+}
